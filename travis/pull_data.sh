2e48567f8604dd4c7a847e949d6a5ca77a0e2c1b
Alan Duarte Carvalhosa <nomeafanaronelbuzon@gmail.com>
2019-08-15 14:30:59 -0300

    asd

--- travis/pull_data.sh
+++ travis/pull_data.sh
@@ -2,11 +2,14 @@
 
 export WORKING_DIR=`pwd`
 echo "> Working dir: $WORKING_DIR"
-eval "$(ssh-agent -s)"
+#eval "$(ssh-agent -s)"
+
+#ssh-add -l 
+ssh -vT aduartebdsol@github.com
 
 
 echo "> Getting data..."
 git clone git@github.com:gss-bds/bds-app-acceptance-tests.git
 
-echo "> Making data dir"
-sudo mv your-data-repo data
+#echo "> Making data dir"
+#sudo mv your-data-repo data
