8e51df58d0bea0d8d26ccba181a55b3567f1da29
Alan Duarte Carvalhosa <nomeafanaronelbuzon@gmail.com>
2019-08-15 14:47:52 -0300

    asd

--- travis/pull_data.sh
+++ travis/pull_data.sh
@@ -4,12 +4,12 @@ export WORKING_DIR=`pwd`
 echo "> Working dir: $WORKING_DIR"
 #eval "$(ssh-agent -s)"
 
-#ssh-add -l 
+ssh-add -l 
 #ssh -vT aduartebdsol@github.com
 
 
 echo "> Getting data..."
-git clone git@github.com:aduartebdsol/Temp-acceptance-test-travis.git
+git clone git@github.com:gss-bds/bds-app-acceptance-tests.git
 
 #echo "> Making data dir"
 #sudo mv your-data-repo data
