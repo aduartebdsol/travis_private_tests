229ed1a8ac08ae25b58c0fbbdd78d147a89eab0a
Alan Duarte Carvalhosa <nomeafanaronelbuzon@gmail.com>
2019-08-15 14:01:57 -0300

    asd

---
+++ pull_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export WORKING_DIR=`pwd`
+echo "> Working dir: $WORKING_DIR"
+
+echo "> Getting data..."
+git clone git@github.com:gss-bds/bds-app-acceptance-tests.git
+
+echo "> Making data dir"
+sudo mv your-data-repo data
